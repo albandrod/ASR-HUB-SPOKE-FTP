763ed699d4b6b606b8951b99d9871b4030a67dd2
swiftsolves-msft <30509195+swiftsolves-msft@users.noreply.github.com>
2019-01-14 12:50:08 -0500

    Add files via upload

---
+++ scripts/applytemppage.ps1
@@ -0,0 +1,7 @@
+﻿Get-Partition -DriveLetter "P"| Set-Partition -NewDriveLetter $TempDriveLetter
+$TempDriveLetter = $TempDriveLetter + ":"
+$drive = Get-WmiObject -Class win32_volume -Filter “DriveLetter = '$TempDriveLetter'”
+#re-enable page file on new Drive
+$drive = Get-WmiObject -Class win32_volume -Filter “DriveLetter = '$TempDriveLetter'”
+Set-WMIInstance -Class Win32_PageFileSetting -Arguments @{ Name = "$TempDriveLetter\pagefile.sys"; MaximumSize = 0; }
+Restart-Computer -Force
